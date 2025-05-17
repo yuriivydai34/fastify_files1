428cd520e81742ee1e955b62e95856048474ea68
Yurii Vydai <yuriivydai@gmail.com>
2025-05-17 17:13:49 +0300

    upload file, next

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,29 @@
+async function saveAction(formData: FormData) {
+  'use server'
+  const file = formData.get('file') as string;
+  console.log(file);
+
+  const response = await fetch(
+    `${process.env.API_URL}/upload`, 
+    {
+      method: "POST",
+      body: formData
+    }
+  )
+
+  console.log(response);
+}
+
 export default async function Home() {
-  const data = await fetch(`${process.env.API_URL}/ping`)
-  const res = await data.text()
+  // const data = await fetch(`${process.env.API_URL}/ping`)
+  // const res = await data.text()
+  
   return (
-    <div><p>{res}</p></div>
+    <div>
+      <form action={saveAction}>
+        <input type="file" name="file" />
+        <input type="submit" />
+      </form>
+    </div>
   );
 }
