a273ccbd2f2e90e25de8ce8730c21e223e32920c
Yurii Vydai <yuriivydai@gmail.com>
2025-05-17 13:05:24 +0300

    fastify multipart

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,14 @@
-import fastify from 'fastify'
+import fastify, { FastifyInstance } from 'fastify'
 
-const server = fastify()
+const server: FastifyInstance = fastify()
 
 import prisma from './client'
 
 import { minioMain } from './minio'
+
+import fastifyMultipart, { MultipartFile } from '@fastify/multipart'
+
+server.register(fastifyMultipart)
 
 server.get('/ping', async (request, reply) => {
   const allUsers = await prisma.user.findMany()
@@ -13,6 +17,19 @@ server.get('/ping', async (request, reply) => {
   minioMain()
   
   return 'pong\n'
+})
+
+server.post('/upload', async (request, reply) => {
+  const data: MultipartFile | undefined = await request.file()
+  let buffer;
+  if (data?.type === 'file') {
+    console.log('filename>>>', data.filename);
+    buffer = await data.toBuffer()
+  }
+
+  console.log('buffer>>>>', buffer);
+
+  reply.send('file uploaded fine!')
 })
 
 server.listen({ port: 8080 }, (err, address) => {
