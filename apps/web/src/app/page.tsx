d87889aebfe5bb84c23b850a30972853eed34363
Yurii Vydai <yuriivydai@gmail.com>
2025-05-18 20:52:42 +0300

    kafka web, react, trying to get messages on front

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,13 @@
 import FilesList from "./filesList";
 import FileUpload from "./fileUpload";
+import KafkaMessages from "./kafkaMessages";
 
 export default async function Home() {
   return (
     <div>
       <FilesList />
       <FileUpload />
+      <KafkaMessages />
     </div>
   );
 }
